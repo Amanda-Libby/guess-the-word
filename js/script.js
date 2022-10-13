2a41c4766d76ebb1564b13b1dcff471ea4eb2ad9
Amanda_L <amandalibbylab@gmail.com>
2022-10-13 16:36:25 -0400

    added global variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector("letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const word = "magnolia";
+
 
 
 
