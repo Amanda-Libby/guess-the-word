82e68b147e0e88346c29f737290ccf4b191ffd20
Amanda_L <amandalibbylab@gmail.com>
2022-12-23 11:52:21 -0500

    completed step 5

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const word = "magnolia";
-const guessedLetters = [];
-const remainingGuesses = 8;
+const playAgainButton = document.querySelector(".play-again");
 
+let word = "magnolia";
+const guessedLetters = [];
+let remainingGuesses = 8;
+
+const getWord = async function () {
+    const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await response.text();
+    const wordArray = words.split("\n");
+    console.log(wordArray);
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+    placeholder(word);
+}; // look at step 5 again since I don't understand the getWord async function and how it works
+
+// fire off the game
+getWord();
 
 const placeholder = function (word) {
     const placeholderLetters = [];
